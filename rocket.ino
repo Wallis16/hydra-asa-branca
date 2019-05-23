3b8b5c66b7c7ea117702d81747185ddebbff7cb2
Diógenes Wallis de França Silva <diogeneswallis@outlook.com>
2019-05-22 22:01:27 -0300

    Add files via upload

--- rocket.ino
+++ rocket.ino
@@ -1,19 +1,10 @@
 #include "TimerOne.h"
 
-//#define ARRAYSIZE 70
-//#define ARRAYSIZE2 140
-
 String g_state = "ST_WAIT_ACK";
+String msg_info = "";
 bool msg, flag1, flag_timer = 0;
 bool g_session_over = 0;
-
-//String ST_names[ARRAYSIZE] = { "ST_INIT", "ST_WAIT_ACK", "ST_SENDING_DATA", "ST_RECEIVING_DATA",
-//                          "desconhecido" };
-
-//String EV_NAMES[ARRAYSIZE2] = {"EV_ACK", "EV_SOT", "EV_TOUT_SOT", "EV_TOUT_SESSION_TX",
-//    "EV_TOUT_NO_RX", "EV_TOUT_SESSION_RX", "EV_EOT", "EV_DADOS", "EV_DATA_SENT",
-//    "EV_NO_DATA", "EV_INVALID", "EV_MAX", "desconhecido"
-//};
+int g_session_over2 = 0;
 
 //criar buffer e flag
 String inputString = "";         // a String to hold incoming data
@@ -25,61 +16,68 @@ String input;
 void setup() {
   Serial.begin(115200);
   inputString.reserve(200);
+
+  msg_info.reserve(200);
+  msg_info = "Hydra\n";
   
-  Timer1.initialize(1000000);         // initialize timer1, and set a 1/2 second period
+  Timer1.initialize(45000);         // initialize timer1, and set a 1/2 second period
   Timer1.attachInterrupt(callback);  // attaches callback() as a timer overflow interrupt
 
-  pinMode(7, INPUT);
 }
 
 void callback()
 {
+  //Serial.print("controle\n");
   if(g_state == "ST_WAIT_ACK"){
     input = "EV_TOUT_SOT\n";
+    Serial.print(input);
   }
   if(g_state == "ST_SENDING_DATA"){
     input = "EV_TOUT_SESSION_TX\n";
   }
-  
+  if(g_state == "ST_RECEIVING_DATA"){
+    input = "EV_TOUT_NO_RX\n";
+  }
+    
   flag_timer = true;
+  //Serial.print("controle\n");
 }
 
 void loop() {
 
+
+    if(msg_info.length() > 0)
+    {
+      msg = false;
+    }
+  
     if (stringComplete == true || flag_timer == true){             
-//Serial.print(inputString);
-    msg = digitalRead(7);
+    //Serial.print(inputString);
+    //msg = digitalRead(7);
     input = inputString;
     inputString = "";
     stringComplete = false;
-    
+    //Serial.print(input);
     if (g_state == "ST_WAIT_ACK"){
-        //timer_tout_sot.resetTimer();
-
-    //flag1 = true;
-  //Serial.print(input);
-        if(input == "EV_TOUT_SOT\n") {
-            //Serial.print("Sot timeout. Reenviando SoT\n");
-            Serial.print("***SoT***\n");
+      
+        if(input == "EV_TOUT_SOT\n") {      
+            Serial.print("SoT -- WAIT_ACK\n");
             input = "";
             flag_timer = false;
-            //send_sot();
+            
             }
-
-        if(input == "EV_ACK\n"){  //7
-            //Serial.print("Recebido EV_ACK.\n");
-            //timer_reset(&g_timer_mgr, SOT_TIMER_ID); //assim que o timer é resetado, necessita ser setado novamente, isso ocorre apenas
-            //Timer1.stop();
-            if(msg == 1) {  //8              // quando send_sot() for requisitada
-                Serial.print("Sending first data after ACK\n");
-                //send_first_data();
+        //Serial.print(input == "EV_ACK\n");
+        delay(2);
+        if(input == "EV_ACK\n"){ 
+            
+            if(msg == 1) {  
+                Serial.print("Hydra -- WAIT_ACK\n");
                 g_state = "ST_SENDING_DATA";
-                Timer1.initialize(100000);
-            } else {      //8
-                Serial.print("No data, sending an EOT\n");
-                //send_eot(); //9
-                g_state = "ST_RECEIVING_DATA"; //10
-                Timer1.initialize(500000);
+                Timer1.initialize(1000000);
+            } else {      
+                Serial.print("EOT -- WAIT_ACK\n");
+                g_state = "ST_RECEIVING_DATA"; 
+                Timer1.initialize(5000000);
             }
         }
         /*else{
@@ -88,53 +86,49 @@ void loop() {
     }
     if(g_state == "ST_SENDING_DATA"){
 
-      //Timer1.initialize(1000000);
         if(input == "EV_TOUT_SESSION_TX\n") {
             g_session_over = 1;
             input = "";
             flag_timer = false;
+            //Serial.print("DALE\n");
         }
-        if(input == "EV_DATA_SENT\n") {
-            if (g_session_over || !msg) {
-                //timer_reset(&g_timer_mgr, DOWNLINK_TIMER_ID);
+        if(g_session_over == 1 || msg == false) {
                 g_session_over = 0;
-                Serial.print("EOT\n");
-                //send_eot();
+                Serial.print("EOT -- SENDING_DATA\n");
                 g_state = "ST_RECEIVING_DATA";
-            } else
+            } 
+         /*else
                   {
-                //send_data();
-                
-                  }
+                    Serial.print("Hydra -- SENDING_DATA\n");
+                    //delay(100);
+                  }*/
         }
         /*else{
         Serial.print("Error-sending-data\n");
             }*/        
-     }
         
     if(g_state == "ST_RECEIVING_DATA"){
         if(input == "EV_TOUT_NO_RX\n") {
-            //timer_reset(&g_timer_mgr, UPLINK_TIMER_ID); //12 reseta o tout_session
-            //send_sot();
+            input = "";
+            flag_timer = false;
+            Serial.print("SoT -- RECEIVING_DATA\n");
+            Timer1.initialize(30000);
             g_state = "ST_WAIT_ACK";
-        }
+            }
+       
         if(input == "EV_EOT\n"){ //11
-            // Resetar o temporizador de sessão
-            if (msg) { //12
-                //timer_reset(&g_timer_mgr, UPLINK_TIMER_ID);
-                //timer_reset(&g_timer_mgr, UPLINK_RX_TIMER_ID);
-                //send_first_data();
+            if (msg == true) { //12
+                Serial.print("Hydra -- RECEIVING_DATA\n");
                 g_state = "ST_SENDING_DATA"; 
-            } else
-                {
-                //send_eot();//13
-                }
+            } 
+            else
+            {
+                Serial.print("EOT -- RECEIVING_DATA\n");
+            }
         }
-        if(input == "EV_DADOS\n"){ //11
+        if(msg == true){ //11
+          //processar dados recebidos
             //processa_dados();
-        }
-        if(input == "EV_TOUT_SESSION_RX\n"){ //11
-            //tout_rx_sessao();
         }
         /*else
         {
